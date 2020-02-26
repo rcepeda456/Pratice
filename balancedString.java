330986c19f8596795e3392da1fedd1e9a3ad1102
Raymond Cepeda <33160305+rcepeda456@users.noreply.github.com>
2020-02-25 19:34:03 -0500

    Add files via upload

---
+++ balancedString.java
@@ -0,0 +1,50 @@
+package pratice;
+
+public class balancedString {
+    int cnt=0;
+    int sum=0;
+    for(int i=0; i<s.length(); i++){
+        if(s.charAt(i)=='L'){
+            sum++;
+        }else{
+            sum--;
+        }
+        
+        if(sum==0){
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
+}
+/*
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
