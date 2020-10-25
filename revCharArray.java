22c6664a897b26734b1617c819bc1d27c4b3c39a
Raymond Cepeda <33160305+rcepeda456@users.noreply.github.com>
2020-10-25 18:05:46 -0400

    Update revCharArray.java

--- revCharArray.java
+++ revCharArray.java
@@ -20,11 +20,11 @@ public class revCharArray {
 	  public void reverseString(char[] s) {
 	    //two varibles , i and j, i equal to s.length and j = to s.length-1, j= s
 	    int j = s.length - 1;
-	    for(int i =0; i<s.length/2; i++){
+	    for(int i =0; i<s.length/2; i++, j--){
 	      char temp = s[i];
 	       s[i]= s[j];
 	       s[j]= temp;
-	      j--;
+	  
 	    }
 	}
 
